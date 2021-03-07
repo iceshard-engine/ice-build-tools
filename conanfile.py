10d2a0733c217663e1a08656826fb1338717d859
Dandielo <dandielo@iceshard.net>
2021-03-07 01:43:47 +0100

    Removed additonal hard coded flags.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class IceBuildToolsConan(ConanFile):
     name = "ice-build-tools"
-    version = "0.4.1"
+    version = "0.4.2"
     license = "MIT"
     description = "IceShard - build tools base"
     url = "https://github.com/iceshard-engine/ice-build-tools"
