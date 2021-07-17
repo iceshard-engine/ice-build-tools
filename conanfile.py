5a7ccb2aedb3a8043e306c731409322a1647f962
Dandielo <dandielo@iceshard.net>
2021-07-17 11:55:16 +0200

    Initial support for UnitTest targets.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class IceBuildToolsConan(ConanFile):
     name = "ice-build-tools"
-    version = "0.4.7"
+    version = "0.4.8"
     license = "MIT"
     description = "IceShard - build tools base"
     url = "https://github.com/iceshard-engine/ice-build-tools"
